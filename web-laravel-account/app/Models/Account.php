aaf5bcaf1f433ae1a1a738dbca880da3731080aa
Zechariah Tan <zechariahtan144@gmail.com>
2022-10-28 11:24:48 +0800

    ✨ create all base files in all microservices

---
+++ web-laravel-account/app/Models/Account.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory, Uuid;
+}
---
+++ web-laravel-debt/app/Models/DebtTransactions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DebtTransactions extends Model
+{
+    use HasFactory, Uuid;
+}
