77779917d1af7654432ad4490fe4724ee6282d1f
Zechariah Tan <zechariahtan144@gmail.com>
2022-10-28 20:33:51 +0800

    ✨ seed database when migrating

--- migrate.ps1
+++ migrate.ps1
@@ -1,7 +1,7 @@
-php web-laravel-authentication/artisan migrate:fresh
-php web-laravel-account/artisan migrate
-php web-laravel-category/artisan migrate
-php web-laravel-transaction/artisan migrate
-php web-laravel-recurrance/artisan migrate
-php web-laravel-debt/artisan migrate
-php web-laravel-budget/artisan migrate
+php web-laravel-authentication/artisan migrate:fresh --seed
+php web-laravel-account/artisan migrate --seed
+php web-laravel-category/artisan migrate --seed
+php web-laravel-transaction/artisan migrate --seed
+php web-laravel-recurrance/artisan migrate --seed
+php web-laravel-debt/artisan migrate --seed
+php web-laravel-budget/artisan migrate --seed
