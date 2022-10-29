63a09361f1c9e8455858fae491c46c563f923c6e
Zechariah <zechariahtan144@gmail.com>
2022-10-29 18:34:07 +0800

    🐛 fix all migrations

--- web-laravel-account/database/migrations/2022_10_28_020220_create_accounts_table.php
+++ web-laravel-account/database/migrations/2022_10_28_020220_create_accounts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('accounts', function (Blueprint $table) {
-            $table->uuid('id');
-            $table->foreignId('user_id')->references('id')->on('users');
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->string('name');
             $table->decimal('balance', 8, 2);
             $table->string('color');
