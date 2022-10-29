ef065620e23d864c2b846bc2b2af8d8be5c82070
Zechariah <zechariahtan144@gmail.com>
2022-10-30 02:11:26 +0800

    ✨ add all routes of transactions

--- web-laravel-transaction/database/factories/TransactionFactory.php
+++ web-laravel-transaction/database/factories/TransactionFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
@@ -16,6 +17,11 @@ class TransactionFactory extends Factory
      */
     public function definition()
     {
-        return [];
+        return [
+            'user_id' => DB::table('users')->first()->id,
+            'amount' => $this->faker->randomFloat(2, 0, 1000),
+            'date' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'description' => $this->faker->sentence,
+        ];
     }
 }
