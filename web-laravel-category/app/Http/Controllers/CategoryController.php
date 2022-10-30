4d513a7cee12ff2cb2dd0595b8e528891e0a4a3a
Zechariah <zechariahtan144@gmail.com>
2022-10-30 22:37:44 +0800

    ✨ return nested categories

--- web-laravel-category/app/Http/Controllers/CategoryController.php
+++ web-laravel-category/app/Http/Controllers/CategoryController.php
@@ -6,25 +6,45 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('owns.category')->only(['show', 'update', 'delete']);
 
         $this->validate('store', [
-            'parent_category_id' => 'exists:categories,id',
+            'parent_category_id' => 'uuid|exists:categories,id',
             'name' => 'required|string',
             'color' => 'required|string',
         ]);
 
         $this->validate('update', [
-            'parent_category_id' => 'exists:categories,id',
+            'parent_category_id' => 'uuid|exists:categories,id',
             'name' => 'string',
             'color' => 'string',
         ]);
     }
 
+    private function getNestedCategory(Category $category)
+    {
+        return [
+            ...$category->toArray(),
+            "categories" => Category::query()
+                ->where("parent_category_id", $category->id)
+                ->get()
+                ->map(function ($category) {
+                    return $this->getNestedCategory($category);
+                }),
+        ];
+    }
+
     public function index()
     {
-        return Category::query()->where('user_id', request()->user_id)->get();
+        return Category::query()
+            ->where('user_id', request()->user_id)
+            ->whereNull('parent_category_id')
+            ->get()
+            ->map(function ($category) {
+                return $this->getNestedCategory($category);
+            });
     }
 
     public function store()
@@ -42,7 +62,7 @@ class CategoryController extends Controller
 
     public function show(Category $category)
     {
-        return $category;
+        return $this->getNestedCategory($category);
     }
 
     public function update(Category $category)
