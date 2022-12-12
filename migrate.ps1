8421e280074c4270534a835de62f466f97550aa3
Zechariah Tan <zechariahtan144@gmail.com>
2022-12-12 22:31:11 +0800

    ✨ use docker compose to run servers

--- migrate.ps1
+++ migrate.ps1
@@ -1,7 +1,4 @@
-php web-laravel-monetary-authentication/artisan migrate:fresh --seed
-php web-laravel-monetary-account/artisan migrate --seed
-php web-laravel-monetary-category/artisan migrate --seed
-php web-laravel-monetary-transaction/artisan migrate --seed
-php web-laravel-monetary-recurrence/artisan migrate --seed
-php web-laravel-monetary-debt/artisan migrate --seed
-php web-laravel-monetary-budget/artisan migrate --seed
+$services = @("account", "authentication", "budget", "category", "debt", "recurrence", "transaction")
+foreach ($service in $services) {
+    php web-laravel-monetary-$service/artisan migrate --seed
+}
