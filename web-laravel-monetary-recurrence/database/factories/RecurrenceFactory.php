e5e014716e1eadbb336fbf31d57004d588e12fb9
Zechariah <zechariahtan144@gmail.com>
2023-01-16 11:15:14 +0800

    ✨ make recurrence factory generate more natural values

--- web-laravel-monetary-recurrence/database/factories/RecurrenceFactory.php
+++ web-laravel-monetary-recurrence/database/factories/RecurrenceFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 /**
@@ -19,7 +20,7 @@ class RecurrenceFactory extends Factory
     {
         $type = $this->faker->randomElement(['Incoming', 'Outgoing', 'Transfer']);
         $fromAccountId = DB::table('accounts')->inRandomOrder()->first()->id;
-        $periodType = $this->faker->randomElement(['Day', 'Week', 'Month', 'Year']);
+        $periodStartDate = Carbon::now()->setMillis(0)->setSeconds(0)->setMinutes(0)->setHours(0);
         $periodEndType = $this->faker->randomElement(['Never', 'Date', 'Count']);
 
         return [
@@ -29,15 +30,15 @@ class RecurrenceFactory extends Factory
             'to_account_id' => $type === 'Transfer' ? DB::table('accounts')->whereNot('id', $fromAccountId)->inRandomOrder()->first()->id : null,
             'type' => $type,
             'name' => $this->faker->word,
-            'amount' => $this->faker->randomFloat(2, 0, 100),
+            'amount' => $this->faker->randomFloat(2, 0, 1000),
             'description' => $this->faker->sentence,
             'automatic' => $this->faker->boolean,
-            'period_start_date' => $this->faker->date(),
-            'period_type' => $periodType,
+            'period_start_date' => $periodStartDate,
+            'period_type' => $this->faker->randomElement(['Day', 'Week', 'Month', 'Year']),
             'period_interval' => $this->faker->numberBetween(1, 4),
             'period_end_type' => $periodEndType,
-            'period_end_date' => $periodEndType === 'Date' ? $this->faker->date() : null,
-            'period_end_count' => $periodEndType === 'Count' ? $this->faker->numberBetween(1, 10) : null,
+            'period_end_date' => $periodEndType === 'Date' ? $periodStartDate->addMonths($this->faker->numberBetween(12, 120)) : null,
+            'period_end_count' => $periodEndType === 'Count' ? $this->faker->numberBetween(10, 100) : null,
             'transaction_ids' => []
         ];
     }
