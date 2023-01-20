23eb9c0508531418d39f57565b4a2bb255e88631
Zechariah <zechariahtan144@gmail.com>
2023-01-20 18:21:52 +0800

    🐛 fix transaction and recurrence model not updating

--- web-laravel-monetary-transaction/app/Models/Transaction.php
+++ web-laravel-monetary-transaction/app/Models/Transaction.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
 class Transaction extends Model
 {
-    use HasFactory, Uuid;
+    use HasFactory, Uuid {
+        boot as public uuidboot;
+    }
 
     protected $fillable = [
         'user_id',
@@ -31,29 +32,15 @@ class Transaction extends Model
         'user_id',
     ];
 
-    public function setTypeAttribute(string $type)
-    {
-        if (array_key_exists('to_account_id', $this->attributes)) {
-            if ($type === "Transfer" && $this->to_account_id === null) {
+    public static function boot() {
+        static::uuidboot();
+        static::saving(function ($model) {
+            if ($model->type === "Transfer" && $model->to_account_id === null) {
                 throw new \Exception("Transfer must have a to_account_id");
             }
-            if ($type !== "Transfer" && $this->to_account_id !== null) {
+            if ($model->type !== "Transfer" && $model->to_account_id !== null) {
                 throw new \Exception("Non-transfer must not have a to_account_id");
             }
-        }
-        $this->attributes['type'] = $type;
-    }
-
-    public function setToAccountIdAttribute(string|null $toAccountId)
-    {
-        if (array_key_exists('type', $this->attributes)) {
-            if ($this->type === "Transfer" && $toAccountId === null) {
-                throw new \Exception("Transfer must have a to_account_id");
-            }
-            if ($this->type !== "Transfer" && $toAccountId !== null) {
-                throw new \Exception("Non-transfer must not have a to_account_id");
-            }
-        }
-        $this->attributes['to_account_id'] = $toAccountId;
+        });
     }
 }
