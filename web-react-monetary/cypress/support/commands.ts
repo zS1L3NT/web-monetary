42d12ca11d940818b2d7cc754398a51e32bf5444
zS1L3NT Arch <zechariahtan144@gmail.com>
2023-01-29 16:32:40 +0800

    ✨ cache jwt token after logging in once

--- web-react-monetary/cypress/support/commands.ts
+++ web-react-monetary/cypress/support/commands.ts
@@ -1,14 +1,28 @@
 /// <reference types="cypress" />
+let token = ""
+
 Cypress.Commands.add("login", (pathname = "/dashboard") => {
+	if (token) {
+		cy.window().then(win => {
+			win.localStorage.setItem("token", token)
+		})
+	}
+
 	cy.visit("http://localhost:8000/login?continue=" + encodeURIComponent(pathname))
 
-	cy.get("[data-cy=email-input]").type("zechariahtan144@gmail.com")
-	cy.get("[data-cy=password-input]").type("P@ssw0rd")
-	cy.get("[data-cy=login-button]").click()
+	if (!token) {
+		cy.get("[data-cy=email-input]").type("zechariahtan144@gmail.com")
+		cy.get("[data-cy=password-input]").type("P@ssw0rd")
+		cy.get("[data-cy=login-button]").click()
+	}
 
 	cy.wait(500)
 
 	cy.location("pathname").should("eq", pathname)
+
+	cy.window().then(win => {
+		token = win.localStorage.getItem("token") as string
+	})
 })
 
 Cypress.Commands.add("push", (path: string) => {
